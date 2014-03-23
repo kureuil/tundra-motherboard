75a8364d669c204e1a0efb2a50dd87e6d7eef3cb
seishynon <sshnn@outlook.com>
2014-03-23 16:15:23 +0100

    Boom. Tile-based map & basic movement done.

--- conf.lua
+++ conf.lua
@@ -2,4 +2,8 @@ function love.conf(t)
 	io.stdout:setvbuf("no")
 	t.console = true
 	t.window.title = "Tundra Motherboard"
+
+	t.window.height = 672
+	t.window.width = 720
+	t.window.borderless = false
 end
--- main.lua
+++ main.lua
@@ -1,3 +1,76 @@
+local class = require 'utils/middleclass'
+
+-- Classe `Player`
+Player = class("Player")
+function Player:initialize(x, y, speed)
+	self.act_x = x
+	self.act_y = y
+	self.grid_x = x
+	self.grid_y = y
+	self.speed = speed
+end
+
+-- Chargement des ressources (maps, sprites, sons, réglages, etc)
+function love.load()
+	-- Tile size: 48px
+	-- Map height: 48 * 10 = 480px
+	-- Map width: 48 * 15 = 720px
+	map = {
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+	}
+	hud_height = 96
+	tile_size = 48
+	player = Player:new(tile_size * 3, tile_size * 5, 10)
+end
+
+-- Mise  à jour des composantes
+function love.update(dt)
+	player.act_x = player.act_x - ((player.act_x - player.grid_x) * player.speed * dt)
+	player.act_y = player.act_y - ((player.act_y - player.grid_y) * player.speed * dt)
+end
+
+-- Détection de pression des touches
+function love.keypressed(key)
+	if key == "down" then
+		local new_y = player.grid_y + tile_size
+		if new_y < (480 + hud_height) then
+			player.grid_y = new_y
+		end
+	elseif key == "up" then
+		local new_y = player.grid_y - tile_size
+		if new_y >= (0 + hud_height) then
+			player.grid_y = new_y
+		end
+	elseif key == "left" then
+		local new_x = player.grid_x - tile_size
+		if new_x >= 0 then
+			player.grid_x = new_x
+		end
+	elseif key == "right" then
+		local new_x = player.grid_x + tile_size
+		if new_x < 720 then
+			player.grid_x = new_x
+		end
+	end
+end
+
+-- Affichage des composantes
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+	for y=1, #map do
+		for x=1, #map[y] do
+			if map[y][x] == 1 then
+				love.graphics.rectangle("line", (x - 1) * tile_size, (y - 1) * tile_size + hud_height, tile_size, tile_size)
+			end
+		end
+	end
+    love.graphics.rectangle("fill", player.act_x, player.act_y, tile_size, tile_size)
 end
