17851b97d4bcb018cbdf7499a4a1afb8ca96face
seishynon <yammal@hotmail.fr>
2014-05-13 21:11:54 +0200

    Update soldier.lua

--- entities/soldier.lua
+++ entities/soldier.lua
@@ -3,13 +3,54 @@ local MovableEntity = require 'entities/movableentity'
 
 local Soldier = class('Soldier', MovableEntity)
 
--- Soldat, actuellement assimilé à une plante verte servant juste à décorée.
+-- Soldat, actuellement assimilé à une plante verte servant juste à décorer.
 function Soldier:initialize()
-
+	self.vision = {}
 end
 
 function Soldier:update(dt)
-	-- body
+	local x = self:getGridX()
+	local y = self:getGridY()
+	
+	if self.direction == 0 then
+		table.insert(self.vision, {x = x, y = y - 3})
+		table.insert(self.vision, {x = x, y = y - 2})
+		table.insert(self.vision, {x = x, y = y - 1})
+		table.insert(self.vision, {x = x + 1, y = y - 2})
+		table.insert(self.vision, {x = x + 1, y = y - 1})
+		table.insert(self.vision, {x = x - 1, y = y - 2})
+		table.insert(self.vision, {x = x - 1, y = y - 1})
+	elseif self.direction == 1 then
+		table.insert(self.vision, {x = x + 3, y = y})
+		table.insert(self.vision, {x = x + 2, y = y})
+		table.insert(self.vision, {x = x + 1, y = y})
+		table.insert(self.vision, {x = x + 1, y = y - 1})
+		table.insert(self.vision, {x = x + 1, y = y + 1})
+		table.insert(self.vision, {x = x + 2, y = y + 1})
+		table.insert(self.vision, {x = x + 2, y = y - 1})
+	elseif self.direction == 2 then
+		table.insert(self.vision, {x = x, y = y + 3})
+		table.insert(self.vision, {x = x, y = y + 2})
+		table.insert(self.vision, {x = x, y = y + 1})
+		table.insert(self.vision, {x = x + 1, y = y + 2})
+		table.insert(self.vision, {x = x + 1, y = y + 1})
+		table.insert(self.vision, {x = x - 1, y = y + 2})
+		table.insert(self.vision, {x = x - 1, y = y + 1})
+	elseif self.direction == 3 then
+		table.insert(self.vision, {x = x - 3, y = y})
+		table.insert(self.vision, {x = x - 2, y = y})
+		table.insert(self.vision, {x = x - 1, y = y})
+		table.insert(self.vision, {x = x - 1, y = y - 1})
+		table.insert(self.vision, {x = x - 1, y = y + 1})
+		table.insert(self.vision, {x = x - 2, y = y + 1})
+		table.insert(self.vision, {x = x - 2, y = y - 1})
+	end
+	
+	for k=1, #self.vision do
+		if self.vision[k].x == map.player:getGridX() and self.vision[k].y == map.player:getGridY() then
+			map:gameOver()
+		end
+	end
 end
 
 -- On dessine un carré rouge pour représenter les soldats.
@@ -18,6 +59,12 @@ function Soldier:draw()
 	local r, g, b, a = love.graphics.getColor()
 	love.graphics.setColor(255, 0, 0)
 	love.graphics.rectangle("fill", self.x, self.y + hud_height, map.tile_size, map.tile_size)
+	love.graphics.setColor(255, 0, 0, 100)
+	for k=1, #self.vision do
+		if self.vision[k].x == map.player:getGridX() and self.vision[k].y == map.player:getGridY() then
+			love.graphics.rectangle("fill", self.vision[k].x * map.tile_size, self.vision[k].y * map.tile_size + hud_height, map.tile_size, map.tile_size)
+		end
+	end
 	-- On rétablit les valeurs des couleurs
 	love.graphics.setColor(r, g, b, a)
 end
